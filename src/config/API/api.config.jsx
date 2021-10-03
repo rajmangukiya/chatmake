46084f7dd6bdc961ed1fe1087530d660d32cb8b1
rajmangukiya <rajmangukiya00@gmail.com>
2021-10-03 21:37:58 +0530

    commit

--- src/config/API/api.config.jsx
+++ src/config/API/api.config.jsx
@@ -4,6 +4,6 @@ const hostname = window.location.hostname;
 const port = window.location.port;
 let isLocalApi =  +port >= 3000;
 
-// export const API = (hostname === 'localhost' && isLocalApi) ? API_LOCAL : API_PROD
+export const API = (hostname === 'localhost' && isLocalApi) ? API_LOCAL : API_PROD
 // export const API = API_PROD;
-export const API = API_LOCAL;
+// export const API = API_LOCAL;
